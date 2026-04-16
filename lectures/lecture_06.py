ad7d46b4ee141843646dbf394f9e54630db3f8b6
Tao Ji <taoji@fudan.edu.cn>
2026-04-16 13:28:43 +0800

    upd lecture 06.

--- lectures/lecture_06.py
+++ lectures/lecture_06.py
@@ -11,6 +11,8 @@ def main():
     text("## 05: 自然语言理解与生成")
     assignment_1()
     how_to_use_lm()
+    how_to_train_lm()
+    assignment_2()
 
     Q_and_A()
 
@@ -32,7 +34,6 @@ def assignment_1():
     link(title="[Getting started with GitHub Copilot CLI]", url="https://docs.github.com/en/copilot/how-tos/copilot-cli/cli-getting-started")
 
 
-
 def how_to_use_lm():
     text("### 如何使用语言模型？")
     text("- 预训练+微调（Pre-training + Fine-tuning）")
@@ -47,6 +48,36 @@ def how_to_use_lm():
 
     text("- 参数学习 vs. 上下文学习（Parameter Learning vs. In-context Learning）")
     image("images/GPT1.png", width=600)
+
+
+def how_to_train_lm():
+    text("### 如何训练语言模型？")
+    text("- 定义一个分词器")
+    text("- 定义一个随机初始化的微型gpt2模型"), link(title="[transformers Models]", url="https://huggingface.co/docs/transformers/index")
+    text("- 找到一个文本数据集")
+    text("- 使用Trainer API训练模型")
+
+
+def assignment_2():
+    text("### 作业2：训练自己的GPT模型 & 参加BabyLM挑战赛")
+
+    text("- 数据集：多语言平行语料"), link(title="[massive]", url="https://huggingface.co/datasets/AmazonScience/massive")
+    text("- **个人作业任务要求**：")
+    text("1. 基于作业1训练的分词器构建微型GPT语言模型。")
+    text("2. 在个人电脑上跑通训练流程")
+    text("3. 对学习率、训练步数、预设词表大小等超参数进行调优，并分析其对训练的影响。")
+    text("4. 实践报告**至少包含两个结论**，多多益善")
+    text("<mark>**DDL：5月20日 24:00**</mark>")
+
+    text("- **团队挑战赛**：")
+    text("1. 以小组为单位参加BabyLM挑战赛（https://babylm.github.io/），使用BabyLM提供的10M预训练数据训练一个语言模型，并获得官方评测结果。")
+    text("2. 本次竞赛可以探讨的方向包括但不限于：")
+    text("-  关心语言：英文、中文、荷兰语的数据比例")
+    text("-  关心数据：高效学习数据形式")
+    text("-  关心架构：更好的分词器、更好的模型架构例如Qwen Gated Attention等"), link(title="[Qwen Gated Attention]", url="https://arxiv.org/pdf/2505.06708")
+    text("-  关心训练：扩散语言模型等新型训练方式"), link(title="[LLaDA]", url="https://arxiv.org/abs/2502.09992")
+    text("-  其他有兴趣的方向")
+    text("自愿报名，组队成功后会提供相关资源和指导，与课程成绩无关，欢迎大家积极参与！")
 
 
 def Q_and_A():
---
+++ lectures/train_my_gpt.py
@@ -0,0 +1,41 @@
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
+from datasets import load_dataset
+
+
+tokenizer = AutoTokenizer.from_pretrained("~/Github/assignment-multilingual-bpe-tokenizer/my_zh_bpe_tokenizer")
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+config = AutoConfig.from_pretrained("gpt2", vocab_size=2000, n_embd=32, n_layer=2, n_head=4)
+my_gpt2 = AutoModelForCausalLM.from_config(config)
+
+# Training loop
+ds = load_dataset("AmazonScience/massive", "zh-CN", split="train", trust_remote_code=True)
+
+def preprocess(example):
+    out = tokenizer(
+        example["utt"],
+        truncation=True,
+        padding="max_length",
+        max_length=64
+    )
+    out["labels"] = out["input_ids"].copy()
+    return out
+
+ds = ds.map(preprocess)
+from transformers import DataCollatorForLanguageModeling
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+# 使用huggingface的Trainer API
+from transformers import Trainer, TrainingArguments
+training_args = TrainingArguments(
+    output_dir="./results",
+    num_train_epochs=1,
+    per_device_train_batch_size=8,
+    logging_dir="./logs",
+    logging_steps=10,
+)
+trainer = Trainer(
+    model=my_gpt2,
+    args=training_args,
+    train_dataset=ds,
+    data_collator=data_collator,
+)
+trainer.train()
